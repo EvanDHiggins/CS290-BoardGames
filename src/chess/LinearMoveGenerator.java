b3b58617480af82db44d2087e45cdcdc416db6b0
EvanDHiggins <evan.higgins06@gmail.com>
2016-04-10 10:28:38 -0500

    Progress on LinearMoveGenerator.

--- src/chess/LinearMoveGenerator.java
+++ src/chess/LinearMoveGenerator.java
@@ -14,10 +14,29 @@ public class LinearMoveGenerator implements IMoveGenerator {
     public Set<Move> generate(GameBoard board, Piece piece) {
         Set<Move> ret = new HashSet<>();
 
-        for(int i = 0; i < board.getSize(); i++) {
-            ret.add(new Move(piece.getPosition(), new Position(piece.getPosition().row(), i)));
-            ret.add(new Move(piece.getPosition(), new Position(i, piece.getPosition().column())));
+//        for(int i = 0; i < board.getSize(); i++) {
+//            ret.add(new Move(piece.getPosition(), new Position(piece.getPosition().row(), i)));
+//            ret.add(new Move(piece.getPosition(), new Position(i, piece.getPosition().column())));
+//        }
+
+        for(int i = 1; i < board.getSize() - 1; i++) {
+            Position to = new Position(piece.getPosition().row(), piece.getPosition().column() + i);
+            if(!board.withinBounds(to))
+                break;
+            Move mv = new Move(piece.getPosition(), to);
+            if(board.pieceAt(to)) {
+                board.getPieceAt(to).map(p -> {
+                    if(!piece.matchesColor(p)) {
+                        mv.setCapture(to);
+                        ret.add(mv);
+                    }
+                    return null;
+                });
+                break;
+            }
+            ret.add(mv);
         }
+
 
         for(Move m : ret) {
             System.out.println(m);
@@ -27,8 +46,6 @@ public class LinearMoveGenerator implements IMoveGenerator {
         System.out.println();
         System.out.println();
 
-        ret = ret.stream().filter(mv -> !mv.getFrom().equals(mv.getTo()))
-                          .collect(Collectors.toSet());
 
         for(Move m : ret) {
             System.out.println(m);
